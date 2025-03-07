a4d209ac4523a925e84f982e48c3d3177849eed0
Stefan Pinto <Stefanhpinto@gmail.com>
2025-03-07 18:46:54 -0500

    html

--- HTML/Javascript/Day 2 - Datatypes/scripts.js
+++ HTML/Javascript/Day 2 - Datatypes/scripts.js
@@ -59,4 +59,91 @@ console.log(string.indexOf("i"))
 //
 console.log("-----")
 
+//last_index_of
+console.log(string.lastIndexOf("script"))
+
 //
+console.log("-----")
+
+//length
+console.log(string.length)
+console.log("There is nothing".length)
+
+//
+console.log("-----")
+
+//match
+let stringy = 'There is only one person'
+let pattern1 = /There/
+console.log(stringy.match(pattern1))
+
+//
+console.log("-----")
+
+//repeat
+console.log("This could be a string or variable".repeat(3))
+
+//
+console.log("-----")
+
+//replace
+let frings = "I like Python"
+console.log(frings)
+console.log(frings.replace('Python', 'Javascript'))
+
+//
+console.log("-----")
+
+//search
+let frings1 = "There could be only one"
+console.log(frings1)
+console.log(frings1.search('one'))
+
+//
+console.log("-----")
+
+//split
+frings2 = "Apple, Banana, Mango, Pineapple, Strawberry"
+console.log(frings2.split(','))
+
+//
+console.log("-----")
+
+//starts_with
+frings3 = "There was an old man from orange county"
+console.log(frings3)
+console.log(frings.startsWith("old"))
+
+//
+console.log("-----")
+
+//substr
+wings = "I like BBQ wings"
+console.log(wings)
+console.log(wings.substr(7, 3))
+
+//
+console.log("-----")
+
+//to_lowercase
+wings1= "SuperDuperLanguage"
+console.log(wings.toLowerCase())
+
+//
+console.log("-----")
+
+//to_UpperCase
+wings2 = "I\'m just slow"
+console.log(wings2.toUpperCase())
+
+//
+console.log("-----")
+
+wings3 = "  There are   too    many  Spaces    "
+console.log(wings3.trim(" "))
+
+//
+console.log("-----")
+
+//typeof
+console.log(typeof false)
--- c sharp/obj/Debug/net5.0/c sharp.AssemblyInfo.cs
+++ c sharp/obj/Debug/net5.0/c sharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("c sharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edb672d44fb0268da162bf6dfe06a07a84f78550")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+850b45bac3e529ab3ed432120005dce384951561")]
 [assembly: System.Reflection.AssemblyProductAttribute("c sharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("c sharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
