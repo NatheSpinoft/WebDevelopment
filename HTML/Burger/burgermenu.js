16ad14c51b912f0ea2ca6315dd32cf20396ce60c
Stefan Pinto <Stefanhpinto@gmail.com>
2025-03-10 18:18:24 -0400

    js and html

---
+++ HTML/Burger/burgermenu.js
@@ -0,0 +1,8 @@
+
+    function changeCSS(style) {
+    // Get the link element by ID
+    var link = document.getElementById("stylesheet");
+
+    // Change the href attribute to point to the new stylesheet
+    link.href = style;
+    }
---
+++ HTML/Javascript/Dump/js001.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+    // Declare the variables
+    var variable = 10;
+    const constant = "John, Smith";
+
+    // Display on HTML
+    document.getElementById('var-output').textContent = "Var value: " + variable;
+    document.getElementById('const-output').textContent = "Const value: " + constant;
+};
+
